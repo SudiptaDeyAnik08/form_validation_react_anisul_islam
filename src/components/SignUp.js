01a0c65f2f7215fd540302125aa7766b0d107458
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-04-21 23:15:57 +0600

    Input validation done

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -14,19 +14,22 @@ const SignUp =()=>{
 
 
             validationSchema:yup.object({
-                userName:yup.string().min(2, "Name Must Contain Atleast 2 charaters").required,
-                userEmail:yup.string().min(2).required,
-                userPassword:yup.string().min(2).required,
+                userName:yup.string().min(2, "Name Must Contain Atleast 2 charaters").required(),
+                userEmail:yup.string().email(2,"Name Must Contain Atleast 6 charaters").required(),
+                userPassword:yup.string().min(6).required(),
 
-            })
+            }),
 
             onSubmit:(values,{resetForm})=>{
                 console.log(values);
                 resetForm({values:""})
-            }
+            },
+
+
         }
     )
 
+    console.log(formik.errors)
 
     // const [userName,setName] = useState('');
     // const [userEmail,setEmail] = useState('');
@@ -62,16 +65,25 @@ const SignUp =()=>{
                <div>
                     <label htmlFor="userName" >Name: </label>
                     <input  type="text" id="userName" name="userName" onChange={formik.handleChange} value={formik.values.userName} />
+                    {
+                         formik.touched.userName&& formik.errors.userName && <span>{formik.errors.userName}</span>
+                    }
                </div>
 
                <div>
                     <label htmlFor="userEmail" >Email: </label>
                     <input  type="text" id="userEmail" name="userEmail" onChange={formik.handleChange} value={formik.values.userEmail}/>
+                    {
+                        formik.touched.userEmail&&  formik.errors.userEmail && <span>{formik.errors.userEmail}</span>
+                    }
                </div>
 
                <div>
                     <label htmlFor="userPassword" >Password: </label>
                     <input  type="password" id="userPassword" name="userPassword" onChange={formik.handleChange } value={formik.values.userPassword} />
+                    {
+                       formik.touched.userPassword&& formik.errors.userPassword && <span>{formik.errors.userPassword}</span>
+                    }
                </div>
 
                <button type="submit">SignUp</button>
